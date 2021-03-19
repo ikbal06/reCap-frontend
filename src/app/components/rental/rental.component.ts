140428783fe86a45dc4713dfe7d05eea6811addb
ikbal kırklar <ikbalkirklar@outlook.com>
2021-03-19 23:23:08 +0300

    17.03.21

--- src/app/components/rental/rental.component.ts
+++ src/app/components/rental/rental.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Rental } from 'src/app/models/entities/rental';
+import { RentalResponseModel } from 'src/app/models/responses/rentalResponseModel';
 import { RentalService } from 'src/app/services/rental.service';
 
 @Component({
@@ -12,13 +13,19 @@ export class RentalComponent implements OnInit {
    rentals:Rental[]=[];
    dataLoaded=false;
 
+   RentalResponseModel:RentalResponseModel={
+    data:this.rentals,
+    message:"",
+    success:true
+  }
+
   constructor(private RentalService:RentalService) { }
 
   ngOnInit(): void {
-   this.getCustomers
+   this.getRentals
   }
 
-  getCustomers(){
+  getRentals(){
     this.RentalService.getRentals().subscribe(response=>{
       this.rentals=response.data
       this.dataLoaded=true;
